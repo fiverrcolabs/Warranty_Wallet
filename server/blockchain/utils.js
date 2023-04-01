b2774caa3bba6fe45f58650ae73d36c980fe5844
Nimsara Fernando <nimsara66@gmail.com>
2023-04-02 01:54:13 +0530

    move secrets to .env

--- server/blockchain/utils.js
+++ server/blockchain/utils.js
@@ -2,6 +2,9 @@ import Web3 from 'web3'
 import axios from 'axios'
 import importedContract from './compiled/WarrantyWallet.json' assert { type: "json" };
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 // TODO: use env
 const createContract = (purchaseDate) => {
     try {
@@ -10,14 +13,14 @@ const createContract = (purchaseDate) => {
         const contractAbi = importedContract.abi
 
         // Set up a web3 provider and contract instance
-        const provider = new Web3.providers.HttpProvider('https://rpc.ankr.com/eth_goerli', {
+        const provider = new Web3.providers.HttpProvider(process.env.RPC_SERVER_ADDRESS, {
             fetch: axios,
         }); // Update with your provider URL
         const web3 = new Web3(provider);
 
         // Set up an account to deploy the contract from
-        const accountAddress = '0x8016a9B7F468467A769379d9DA4A1695aabe208C'; // Update with your account address
-        const privateKey = '0x3a1f0834b73a1dddb06667af36bd6c406530d4169ff4e832339ae430743e5e3e'; // Update with your account's private key
+        const accountAddress = process.env.METAMASK_ADDRESS; // Update with your account address
+        const privateKey = process.env.METAMASK_PRIVATE_KEY; // Update with your account's private key
         const account = web3.eth.accounts.wallet.add(privateKey);
 
         // Get hash of input
@@ -40,7 +43,7 @@ const useContract = (contractAddress, purchaseDate) => {
         const contractAbi = importedContract.abi
 
         // Set up a web3 provider and contract instance
-        const provider = new Web3.providers.HttpProvider('https://rpc.ankr.com/eth_goerli', {
+        const provider = new Web3.providers.HttpProvider(process.env.RPC_SERVER_ADDRESS, {
             fetch: axios,
         }); // Update with your provider URL
         const web3 = new Web3(provider);
@@ -48,7 +51,7 @@ const useContract = (contractAddress, purchaseDate) => {
         const myContract = new web3.eth.Contract(contractAbi, contractAddress);
 
         const hash = web3.utils.sha3(purchaseDate)
-        return myContract.methods.verifyHash(hash).call({ from: '0x8016a9B7F468467A769379d9DA4A1695aabe208C' })
+        return myContract.methods.verifyHash(hash).call({ from: process.env.METAMASK_ADDRESS })
     } catch (error) {
         console.log('Error using contract method:', error)
     }
